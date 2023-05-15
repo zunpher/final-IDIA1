a7facf606a9d723326a94920f134b994dc20c040
zunpher <pattiralekwilai@gmail.com>
2023-05-15 16:31:07 +0700

    1st

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Final {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter max water capacity: ");
+            double vMax = double.Parse(Console.ReadLine());
+            Console.Write("Enter average water volume: ");
+            double vFill = double.Parse(Console.ReadLine());
+            Console.Write("Enter number of drinking water: ");
+            double vDrink = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter event time: ");
+            double tDay = double.Parse(Console.ReadLine());
+            Console.Write("Enter round of filling water: ");
+            double tFill = double.Parse(Console.ReadLine());
+            Console.Write("Enter round of drink: ");
+            double tDrink = double.Parse(Console.ReadLine());
+
+            while (vDrink < vMax) {
+                
+            }
+        }
+    }
+}
